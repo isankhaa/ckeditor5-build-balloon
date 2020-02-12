0aec81eba0ca7bb983a473b0318666b553278ee6
Ankhbayar <ankhbayar@infinite.mn>
2020-02-12 15:32:37 +0800

    font

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,31 +7,23 @@
 import BalloonEditorBase from "@ckeditor/ckeditor5-editor-balloon/src/ballooneditor";
 
 import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials";
-// import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat";
 import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold";
 import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic";
-// import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-// import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-// import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from "@ckeditor/ckeditor5-heading/src/heading";
-// import Image from '@ckeditor/ckeditor5-image/src/image';
-// import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
-// import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
-// import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
-// import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
 import Indent from "@ckeditor/ckeditor5-indent/src/indent";
 import Link from "@ckeditor/ckeditor5-link/src/link";
 import List from "@ckeditor/ckeditor5-list/src/list";
-// import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
 import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph";
 import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice";
 import Table from "@ckeditor/ckeditor5-table/src/table";
 import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar";
 
 //ankhaa
-import Highlight from "@ckeditor/ckeditor5-highlight/src/highlight";
 import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment";
+import RemoveFormat from "@ckeditor/ckeditor5-remove-format/src/removeformat";
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline";
+import Font from "@ckeditor/ckeditor5-font/src/font";
 
 export default class BalloonEditor extends BalloonEditorBase {}
 
@@ -39,7 +31,9 @@ export default class BalloonEditor extends BalloonEditorBase {}
 BalloonEditor.builtinPlugins = [
 	Essentials,
 	Alignment,
-	Highlight,
+	RemoveFormat,
+	Underline,
+	Font,
 	// UploadAdapter,
 	Autoformat,
 	Bold,
@@ -71,10 +65,16 @@ BalloonEditor.defaultConfig = {
 			"|",
 			"bold",
 			"italic",
+			"underline",
 			"link",
 			"|",
+			"fontSize",
+			"fontFamily",
+			"fontColor",
+			"|",
 			"alignment",
-			"highlight",
+			"|",
+			"removeFormat",
 			"|",
 			"bulletedList",
 			"numberedList",
